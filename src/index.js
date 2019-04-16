67d58a87a6ed3009309fc07fe68d2bbd7da44c98
k-kimura <kimura_511@me.com>
2019-04-16 22:00:52 +0900

    install webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 function component() {
   const element = document.createElement('div');
-  const array = ['hello', 'webpack'];
+  const array = ['hello', 'webpack', '!!!11'];
   element.innerHTML = _.join(array, '');
   return element;
 }
