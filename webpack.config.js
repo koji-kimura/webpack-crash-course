ade2bb1b8d293897882a25200b7e02f768a7acd1
k-kimura <kimura_511@me.com>
2019-04-25 07:04:32 +0900

    implement babel-loader and react development

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const outputPath = path.resolve(__dirname, 'dist');
 module.exports = {
   entry: './src/index.js',
@@ -12,6 +12,8 @@ module.exports = {
       // cssを指定する
       // 余計な記載が入っていたのが原因
       // loaderはタグ順に動く、後ろから順番に動く、まずスタイルの適応、その後でcssの適応が正しい
+      // 最後から順番に実行される
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
       { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
       {
@@ -23,11 +25,18 @@ module.exports = {
           // 分離する場所
           name: './images/[name].[ext]'
         }
-      }
+      },
+      { test: /\.html$/, loader: 'html-loader' }
     ]
   },
   //documentrootの追加
   devServer: {
     contentBase: outputPath
-  }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
 };
